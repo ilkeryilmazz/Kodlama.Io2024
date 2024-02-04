9598873491398e4950e7a68e9ba071614274c7cb
Monster <Monster@MONSTER>
2024-02-04 23:08:12 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,41 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business
+{
+    public class CourseManager
+    {
+        Course[] courses = new Course[3];
+        public CourseManager()
+        {
+            Course course1 = new Course();
+            Course course2 = new Course();
+            Course course3 = new Course();
+
+            course1.Id = 1;
+            course1.Name = "C#";
+            course1.Description = ".NET 8";
+            course1.Price = 10;
+
+
+            course2.Id = 2;
+            course2.Name = "JAVA";
+            course2.Description = " JAVA 17";
+            course2.Price = 20;
+
+            course3.Id = 3;
+            course3.Name = "Python";
+            course3.Description = "Python 3.12";
+            course3.Price = 20;
+            courses[0] = course1; courses[1]=course2 ; courses[2]=course3 ;
+        }
+        public Course[] GetAll()
+        {
+            return courses;
+        }
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+
+//variables --> camelCase
+bool isAuthenticated = false;
+Console.WriteLine(message1);
+
+//condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Hoşgeldin Engin");
+}
+else
+{
+    Console.WriteLine("Sisteme giriş yap");
+}
+
+
+
+
+string[] loans = {"Kredi 1", "Kredi 2","Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6"};
+//string[] loans2 = new string[6];
+//loans2[0]="Kredi 1";
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+
+
+
+
+CourseManager courseManager = new();
+
+var data = courseManager.GetAll();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i].Name + " / " + data[i].Price);
+}
+
+
+Console.WriteLine("Kod bitti");
